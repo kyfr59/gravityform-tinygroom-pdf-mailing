507f60f1dfe3a65c7ba9ebad8336160cccc05b89
Franck Dupont <kyfr59@gmail.com>
2019-12-02 16:17:37 +0100

    Create the table to store form mailing choices

--- gravityforms-tinygroom-pdf-mailing.php
+++ gravityforms-tinygroom-pdf-mailing.php
@@ -4,7 +4,7 @@
  * @wordpress-plugin
  * Plugin Name:       Gravity Forms Tinygroom PDF mailing
  * Description:       Generate PDF exports of Tinygroom forms for papers mailing
- * Version:           1.0.0
+ * Version:           1.1.0
  * Author:            Franck Dupont
  * Author URI:        http://franck-dupont.me
  * License:           GPL-2.0+
@@ -23,9 +23,24 @@ if (!class_exists('GravityForms_Tinygroom_PDF_Mailing_Plugin')) {
 
 		function __construct() {
 			$this->run();
+			register_activation_hook(__FILE__, array($this, 'install'));
 		}
 
-		function install() { }
+		function install() {
+
+			global $wpdb;
+			$table_name = $wpdb->prefix . "gf_tinygroom_pdf_mailing";
+			$charset_collate = $wpdb->get_charset_collate();
+
+			$sql = "CREATE TABLE `$table_name` (
+				form_id mediumint(9) NOT NULL,
+				post_values text NOT NULL,
+			  PRIMARY KEY  (form_id)
+			) $charset_collate;";
+
+			require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+			dbDelta( $sql );
+		}
 
 		function uninstall() { }
 
